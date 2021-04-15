a4e9fa48a5fdffe128b7eaa0d14bc92f6a39cd1b
Mahdi <mahdibouaziz@hotmail.fr>
2021-04-15 11:04:05 +0000

    repository pattern

--- src/cv/cv.controller.ts
+++ src/cv/cv.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CvService } from './cv.service';
 
 @Controller('cv')
-export class CvController {}
+export class CvController {
+  constructor(private cvService: CvService) {}
+
+  @Get()
+  async getCvs() {
+    return await this.cvService.getCVs();
+  }
+}
--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CvEntity } from './entities/cv.entity';
 
 @Injectable()
-export class CvService {}
+export class CvService {
+  constructor(
+    @InjectRepository(CvEntity)
+    private cvRepository: Repository<CvEntity>,
+  ) {}
+
+  async getCVs(): Promise<CvEntity[]> {
+    return await this.cvRepository.find();
+  }
+}
